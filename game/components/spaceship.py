2f345e0321e0bf8089bb02ca000c9fc0db055f0e
SergioSanga <serginomartinez73@gmail.com>
2023-07-17 20:51:06 -0400

    First-Class-Task

--- game/components/spaceship.py
+++ game/components/spaceship.py
@@ -6,10 +6,12 @@ from game.utils.constants import SPACESHIP, SCREEN_HEIGHT, SCREEN_WIDTH
 class Spaceship(Sprite):
     SPACESHIP_WIDTH = 40
     SPACESHIP_HEIGHT = 60
-    X_POS = (SCREEN_WIDTH//2) - SPACESHIP_WIDTH//2
+    X_POS = (SCREEN_WIDTH // 2) - (SPACESHIP_WIDTH // 2)
     Y_POS = 500
+    SPEED = 10
 
     def __init__(self):
+        super().__init__()
         self.image = pygame.transform.scale(SPACESHIP, (self.SPACESHIP_WIDTH, self.SPACESHIP_HEIGHT))
         self.rect = self.image.get_rect()
         self.rect.x = self.X_POS
@@ -17,11 +19,19 @@ class Spaceship(Sprite):
 
     def update(self, user_input):
         if user_input[pygame.K_LEFT]:
-            if self.rect.left > 0:
-                self.rect.x -= 10
+            self.rect.x -= self.SPEED
+            if self.rect.right <= 0:
+                self.rect.x = SCREEN_WIDTH
         elif user_input[pygame.K_RIGHT]:
-            if self.rect.right < SCREEN_WIDTH:
-                self.rect.x += 10
+            self.rect.x += self.SPEED
+            if self.rect.left >= SCREEN_WIDTH:
+                self.rect.x = -self.SPACESHIP_WIDTH
+        elif user_input[pygame.K_UP]:
+            if self.rect.y > SCREEN_HEIGHT // 2:
+                self.rect.y -= self.SPEED
+        elif user_input[pygame.K_DOWN]:
+            if self.rect.y < SCREEN_HEIGHT - self.SPACESHIP_HEIGHT:
+                self.rect.y += self.SPEED
 
     def draw(self, screen):
         screen.blit(self.image, (self.rect.x, self.rect.y))
